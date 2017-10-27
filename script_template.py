5c2085690aafa678123d2c5fe4f5891a3cb2a0d0
Li-Wei Hung <li-wei_hung@nps.gov>
2017-10-27 09:31:43 -0600

    adding the script template

---
+++ script_template.py
@@ -0,0 +1,47 @@
+#-----------------------------------------------------------------------------#
+#?????.py
+#
+#NPS Night Skies Program
+#
+#Last updated: 2017/??/??
+#
+#This script...
+#	(1) 
+#	(2)  
+#	(3) 
+#   (4) 
+#
+#Note: 
+#
+#
+#Input: 
+#   (1) 
+#   (2) 
+#   (3) 
+#
+#Output:
+#   (1) 
+#   (2) 
+#
+#History:
+#	Dan Duriscoe -- Created in visual basic as ""
+#	Li-Wei Hung -- Cleaned and translated to python
+#
+#-----------------------------------------------------------------------------#
+
+#from astropy.io import fits
+#from datetime import datetime as Dtime
+#from glob import glob, iglob
+#from scipy.misc import imsave 
+#from win32com.client import Dispatch
+
+#import pdb
+#import matplotlib.pyplot as plt
+#import numpy as n
+#import os
+#import shutil
+
+# Local Source
+import filepath  
+
+#-----------------------------------------------------------------------------#
